dbad89e0d3e787626a0512679d57427249ac17aa
Clemens Bartz <clemens@clemens-bartz.de>
2015-04-05 14:20:49 +0200

    Rewrite code
    - Create modules for add on code
    - Add settings page (issue #16, issue #17)
    - Change pagemod creation
    - Remove option to block resources
    - Add option to block data URLs
    - Simplify blocking message
    - Update German translation

---
+++ data/settings.js
@@ -0,0 +1,166 @@
+const showForm = document.getElementById("showForm");
+const rules = document.getElementById("rules");
+const form = document.getElementById("form");
+const cancel = document.getElementById("cancel");
+const rule = document.getElementById("rule");
+const message = document.getElementById("message");
+const addRule = document.getElementById("addRule");
+const rulesList = document.getElementById("rulesList");
+const removeList = document.getElementById("removeList");
+const remove = document.getElementById("remove"); 
+
+function isExactMatch(pattern) {
+	const urlPattern =
+		new RegExp("^(http|ftp|https)://[\\w-]+(\\.[\\w-]+)*/$");
+	return urlPattern.test(pattern);
+}
+
+
+function isDomainNamePrefixedWithAsterixAndDot(pattern) {
+	const urlPattern = new RegExp("^\\*(\\.[\\w-]+)+$");
+	return urlPattern.test(pattern);
+}
+
+
+function isURLFollowedByAsterix(pattern) {
+	const urlPattern =
+		new RegExp("^(http|ftp|https)://[\\w-]+(\\.[\\w-]+)*/\\*$");
+	return urlPattern.test(pattern);
+}
+
+
+function changeMessage(color, msg) {
+	message.style.borderColor = color;
+	message.innerHTML = msg;
+}
+
+
+const onRuleChange = function(data) {
+	rule.value = rule.value.trim();
+
+	if (rule.value == null) {
+		return;
+	}
+
+	if (rule.value == "") {
+		changeMessage("red", "Please enter a new rule.");
+		return;
+	}
+
+	if ((rule.value.match(/\*/g) || []).length > 1) {
+		changeMessage("red", "There can only be one asterix (*) in a rule.");
+		return;
+	}
+
+	let go = false;
+
+	if (isExactMatch(rule.value)) {
+		go = true;
+		changeMessage("green", "Only this URL will be blocked.");
+	}
+
+	if (isDomainNamePrefixedWithAsterixAndDot(rule.value)) {
+		go = true;
+		changeMessage("green", "All URLs and subpages containing the domain" + 
+			" will be blocked.");
+	}
+
+	if (isURLFollowedByAsterix(rule.value)) {
+		go = true;
+		changeMessage("green", "All URLs and subpages will be blocked.");
+	}
+
+	if (go) {
+		addRule.style.display = "inline";
+	}
+	else {
+		changeMessage("red", "This is not a correct rule.");
+		addRule.style.display = "none";
+	}
+};
+
+
+const onCancelClick = function(data) {
+	rules.style.display = "block";
+	form.style.display = "none";
+
+	rule.value = "";
+	onRuleChange(false);
+};
+
+
+const onShowFormClick = function(data) {
+	rules.style.display = "none";
+	form.style.display = "block";
+};
+
+
+const onGetRules = function(data) {
+	rulesList.innerHTML = "";
+	removeList.innerHTML = "";
+
+	if (data.length == 0) {
+		const li = document.createElement("li");
+		li.appendChild(document.createTextNode("There are no rules, yet."));
+		rulesList.appendChild(li);
+
+		remove.style.display = "none";
+
+		return;
+	}
+
+	for each (let rule in data) {
+		const li = document.createElement("li");
+		li.appendChild(document.createTextNode(rule));
+		rulesList.appendChild(li);
+
+		const option = document.createElement("option");
+		option.appendChild(document.createTextNode(rule));
+		option.value = rule;
+		removeList.appendChild(option);
+	}
+
+	remove.style.display = "inline";
+};
+
+
+const onAddRuleClick = function(data) {
+	self.port.emit("add", rule.value);
+
+	onCancelClick(false);
+}
+
+
+const onRemoveClick = function(data) {
+	if (removeList.selectedIndex < 0) {
+		return;
+	}
+
+	self.port.emit("remove", 
+		removeList.options[removeList.selectedIndex].value);
+}
+
+if (rule != null) {
+	rule.addEventListener("change", onRuleChange);
+	rule.addEventListener("keyup", onRuleChange);
+}
+
+if (cancel != null) {
+	cancel.addEventListener("click", onCancelClick);
+}
+
+if (showForm != null) {
+	showForm.addEventListener("click", onShowFormClick);
+}
+
+if (addRule != null) {
+	addRule.addEventListener("click", onAddRuleClick);
+}
+
+if (remove != null) {
+	remove.addEventListener("click", onRemoveClick);
+}
+
+self.port.on("getRules", onGetRules);
+
+addRule.style.display = "none";
